123ffd0a72edab655082c7e6f52f4d38f1a1d813
hongbing911 <407234756@qq.com>
2017-02-06 12:00:03 +0800

    ass test file

---
+++ TPL/phpClass0206.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/2/6
+ * Time: 11:57
+ */
+
+$user = new User();
+print_r($user);
+
+class User{
+    public $user, $password;
+}
