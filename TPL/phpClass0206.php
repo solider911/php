330e1413e8c6146d65cc761cdd1d8b5e6456697f
hongbing911 <407234756@qq.com>
2017-02-06 14:53:50 +0800

    test 01

--- TPL/phpClass0206.php
+++ TPL/phpClass0206.php
@@ -8,7 +8,40 @@
 
 $user = new User();
 print_r($user);
+echo "<br>";
+
+$user->user = "cxxxxxx";
+$user->password = "xsadfsadf";
+print_r($user);
+echo "<br>";
+
+$user->save_user();
+echo "<br>";
+
+
+Translate::lookup();
+echo "<br>";
+
+$translate = new Translate();
+$translate->lookup2();
+
 
 class User{
     public $user, $password;
+    function save_user(){
+        echo "Save User code here";
+    }
+}
+
+class Translate{
+    const ENGLISH = 0;
+    const FRENCH  = 1;
+
+    static function lookup(){
+        echo self::FRENCH;
+    }
+
+    function lookup2(){
+        echo self::FRENCH;
+    }
 }
